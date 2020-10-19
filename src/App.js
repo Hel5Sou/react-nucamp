6bdc220d7c0f01c6349ded1021135a3d3fb65286
Helena <helena.soukupova5@gmail.com>
2020-10-18 18:02:28 -0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryCompoment';
+import Main from './components/MainComponent';
 import './App.css';
-import { CAMPSITES} from './shared/campsites';
-
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        }
-    }
-    
-  render() {
-      return (
-          <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory campsites={this.state.campsites} /> 
-          </div>
-      );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Main />
+            </div>
+        );
+    };
 }
 
-export default App ;
+export default App;
--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -48,13 +48,15 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return (
-            <div className="row">
-                 {this.renderCampsite(this.props.campsite)}
-                 {this.renderComments(this.props.campsite.comments)}
-            </div>
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
             );
         }
-        return <div />
+        return <div />;
     }
 }
 

9d42c5c9b8495beca71918212d7cc042b13b4383
Helena <helena.soukupova5@gmail.com>
2020-10-22 15:48:23 -0700

    React Router Parameters

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -48,7 +48,7 @@ import {Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                 <div className="container">
                     <div className="row">
                         <RenderCampsite campsite ={props.campsite} />
-                        <RenderComments comments={props.campsite.comments} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -36,6 +36,15 @@ class Main extends Component {
                 />
             );
         };
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        }; 
     
         return (
             <div>
@@ -43,6 +52,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exacp path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
